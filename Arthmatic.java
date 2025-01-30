a3b71f20ed91565b78bf022d20a58916a259411a
Poojitha366 <231801410020@cutmap.ac.in>
2025-01-30 19:07:24 +0530

    Add files via upload

---
+++ Arthmatic.java
@@ -0,0 +1,17 @@
+class ArithmaticOperator{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int add=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int mod=a%b;
+        System.out.println("Add:"+add);
+        System.out.println("Sub:"+sub);
+        System.out.println("Mul:"+mul);
+        System.out.println("Div:"+div);
+        System.out.println("Mod:"+mod);
+        
+    }
+}
