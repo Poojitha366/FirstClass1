6db80dc61ae91f20331ac49d670f7a2b249f99ce
Poojitha366 <231801410020@cutmap.ac.in>
2025-02-05 14:38:12 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        int id=20;
+        String name="Poojitha";
+        double marks = 20.5;
+        System.out.println("id"+" "  +id +" " +"Name"+" "  +name+" " +"Marks"+" " +marks);
+    }
+}
